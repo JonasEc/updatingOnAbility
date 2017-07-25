0055eebfecce20a4f3879a0325eb12ca20299d36
Jonas MG <jonasmg@stanford.edu>
2017-07-25 14:05:13 -0700

    First Test Deploy

---
+++ updatingOnAbility/models.py
@@ -0,0 +1,29 @@
+from otree.api import (
+	models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+	Currency as c, currency_range
+)
+
+
+author = 'Jonas Mueller Gastell & Yucheng Liang'
+
+doc = """
+Experiment to Test Updating on Ability
+"""
+
+
+class Constants(BaseConstants):
+	name_in_url = 'experiment'
+	players_per_group = None
+	num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+	pass
+
+
+class Group(BaseGroup):
+	pass
+
+
+class Player(BasePlayer):
+	pass
