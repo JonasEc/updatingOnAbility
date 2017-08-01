82d8edc503ac0babc34b4aea6d63efc6ecafa129
Jonas MG <jonasmg@stanford.edu>
2017-07-31 18:20:19 -0700

    Info Demand First Ugly Draft

--- updatingOnAbility_updating/models.py
+++ updatingOnAbility_updating/models.py
@@ -15,11 +15,11 @@ Experiment to Test Updating on Ability
 
 
 class Constants(BaseConstants):
-	name_in_url = 'experiment'
+	name_in_url = 'experiment2'
 	players_per_group = None
 	num_rounds = 4
 
-	# Here I define the different infor structures:
+	# Here I define the different information structures:
 	# naming convention: AccNameNSignal << Acc for Accuracy, Name for structure name, N for iteration of structure if applicable, Signal for whether good or bad 
 	AccAsym1Good = 0.85 # if in upper half, get good signal with AccAsym1Good % probability
 	AccAsym1Bad = 0.6 # if in lower half, get bad signal with AccAsym1Bad % probability
@@ -32,10 +32,22 @@ class Constants(BaseConstants):
 
 	# no info: 0.5 for both; full info: 1 for both
 
+	# Next I define what these will be called in the templates
+	NameDict = {"Certain": "Fully informative urns", "Imperfect": "Partially informative urns", "NoInfo": "Uninformative urns", "Asym1": "Positively Skewed urns", "Asym2": "Negatively Skewed urns"}
 
 	# Here i choose the range for elicitation:
-	maxLower = -3
-	maxUpper = -3
+	maxUpper = 2
+	fineUpper = 0.75
+	maxLower = -maxUpper
+	fineLower = -fineUpper
+	steps = 11
+	steps2 = 6
+	stepsize = (maxUpper - fineUpper)/(steps2 - 1)
+	rangeOverFine = np.linspace(fineLower, fineUpper, steps, endpoint = True)
+	rangeRoughLower = np.linspace(maxLower, fineLower - stepsize, (steps2-1), endpoint = True)
+	rangeRoughUpper = np.linspace(fineUpper + stepsize, maxUpper, (steps2-1), endpoint = True)
+
+
 
 class Subsession(BaseSubsession):
 	pass
@@ -47,34 +59,38 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 	
-	#### Demand for Information:
+	#### Demand for	:
 
 	# Here, I make four pairwise comparison choice variables that capture the elicited willingness to pay that will always be displayed
-	CertainVSNoInfo    = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	CertainVSImperfect = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	ImperfectVSNoInfo  = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	Asym1VSAsym2       = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
+	CertainVSNoInfo    = models.CurrencyField()
+	CertainVSImperfect = models.CurrencyField()
+	ImperfectVSNoInfo  = models.CurrencyField()
+	Asym1VSAsym2       = models.CurrencyField()
 	
+	# Next, the variable choice
+	VarChoice = models.CurrencyField()
+
+
 	# Next, I make the four possible choices, one of which will be displayed
-	CertainVSAsym1   = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	ImperfectVSAsym1 = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	NoInfoVSAsym1    = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	CertainVSAsym2   = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	ImperfectVSAsym2 = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)
-	NoInfoVSAsym2    = models.CurrencyField(widget = widgets.SliderInput(attrs={'step': '0.05'}), min = maxLower, max = maxUpper)	
+	# CertainVSAsym1   = models.CurrencyField()
+	# ImperfectVSAsym1 = models.CurrencyField()
+	# NoInfoVSAsym1    = models.CurrencyField()
+	# CertainVSAsym2   = models.CurrencyField()
+	# ImperfectVSAsym2 = models.CurrencyField()
+	# NoInfoVSAsym2    = models.CurrencyField()	
 
 
 	# these work as follows: random Value between maxLower and maxUpper. If Value is above choice of player, implement RightHandSide info structure and give player +Value payoff. 
 	# Otherwise give player LHS info structure and no change to payment. Lottery is between LHS and RHS+PaymentAmount
 	# ie: default is LHS, payment/deduction is to get/avoid RHS.
-	intransitiveChoice = model.BooleanField()
+	intransitiveChoice = models.BooleanField()
 
 	def VariableChoiceChooser(CertainVSNoInfo, CertainVSImperfect, ImperfectVSNoInfo, Asym1VSAsym2):
 		if Asym1VSAsym2 > c(0): # then Asym1 is preferred over Asym2
 			max1 = "Asym2"
 		elif Asym1VSAsym2 < c(0): 
 			max1 = "Asym1"
-		else Asym1VSAsym2 == c(0): 
+		else: 
 			max1 = random.choice(["Asym1", "Asym2"])
 
 		
@@ -96,6 +112,6 @@ class Player(BasePlayer):
 			else:
 				max2 = "Imperfect"
 
-		return max1 max2 intransitive
+		return max1, max2, intransitive
 
 			
--- updatingOnAbility_updating/views.py
+++ updatingOnAbility_updating/views.py
@@ -2,18 +2,54 @@ from otree.api import Currency as c, currency_range
 from . import models
 from ._builtin import Page, WaitPage
 from .models import Constants
-
+import numpy as np
 
 class info1(Page):
 	form_model = models.Player
-	form_fields = ['attentioncheck']
+	form_fields = ['CertainVSNoInfo']
+	def vars_for_template(self):
+		rangeOver = np.concatenate((Constants.rangeRoughLower,Constants.rangeOverFine,Constants.rangeRoughUpper))
+		right_side_amounts =[c(rangeOver[k]) for k in range(len(rangeOver))]
+		return {"left_side": Constants.NameDict["Certain"], "right_side": Constants.NameDict["NoInfo"], "right_side_amounts": right_side_amounts}	
 
 class info2(Page):
 	form_model = models.Player
-	form_fields = ['attentioncheck']
+	form_fields = ['CertainVSImperfect']
+	def vars_for_template(self):
+		rangeOver = np.concatenate((Constants.rangeRoughLower,Constants.rangeOverFine,Constants.rangeRoughUpper))
+		right_side_amounts =[c(rangeOver[k]) for k in range(len(rangeOver))]
+		return {"left_side": Constants.NameDict["Certain"], "right_side": Constants.NameDict["Imperfect"], "right_side_amounts": right_side_amounts}	
 
+class info3(Page):
+	form_model = models.Player
+	form_fields = ['ImperfectVSNoInfo']
+	def vars_for_template(self):
+		rangeOver = np.concatenate((Constants.rangeRoughLower,Constants.rangeOverFine,Constants.rangeRoughUpper))
+		right_side_amounts =[c(rangeOver[k]) for k in range(len(rangeOver))]
+		return {"left_side": Constants.NameDict["Imperfect"], "right_side": Constants.NameDict["NoInfo"], "right_side_amounts": right_side_amounts}	
+
+class info4(Page):
+	form_model = models.Player
+	form_fields = ['Asym1VSAsym2']
+	def vars_for_template(self):
+		rangeOver = np.concatenate((Constants.rangeRoughLower,Constants.rangeOverFine,Constants.rangeRoughUpper))
+		right_side_amounts =[c(rangeOver[k]) for k in range(len(rangeOver))]
+		return {"left_side": Constants.NameDict["Asym1"], "right_side": Constants.NameDict["Asym2"], "right_side_amounts": right_side_amounts}	
+
+class info5(Page):
+	form_model = models.Player
+	form_fields = ['VarChoice']
+	def vars_for_template(self):
+		rangeOver = np.concatenate((Constants.rangeRoughLower,Constants.rangeOverFine,Constants.rangeRoughUpper))
+		right_side_amounts =[c(rangeOver[k]) for k in range(len(rangeOver))]
+		max1, max2, intransitive = self.player.VariableChoiceChooser(self.player.CertainVSNoInfo, self.player.CertainVSImperfect, self.player.ImperfectVSNoInfo, self.player.Asym1VSAsym2)	
+		return {"left_side": Constants.NameDict(max1), "right_side": Constants.NameDict(max2), "right_side_amounts": right_side_amounts}	
 
 page_sequence = [
 	info1,
 	info2,
+	info3,
+	info4,
+	info5,
 ]
+
