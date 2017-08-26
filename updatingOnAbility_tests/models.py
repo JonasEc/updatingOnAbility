53f4581fdf9b6ed29be5ac6b88654902e44e2b94
Yucheng‘s Mac <Yucheng@DN0a23250b.SUNet>
2017-08-26 16:52:33 -0700

    raven test added

--- updatingOnAbility_tests/models.py
+++ updatingOnAbility_tests/models.py
@@ -2,7 +2,8 @@ from otree.api import (
 	models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
 	Currency as c, currency_range
 )
-
+import random as r
+import csv
 
 author = 'Jonas Mueller Gastell & Yucheng Liang'
 
@@ -15,11 +16,31 @@ class Constants(BaseConstants):
 	name_in_url = 'experiment'
 	players_per_group = None
 	num_rounds = 1
-	test = "test"
+	test = "raven"
+	test_minutes = 5
 
+	correct_point = 3
+	wrong_point = -1
+
+	with open('updatingOnAbility_tests/raven.csv') as f:
+		questions = list(csv.DictReader(f))
+
+	num_rounds = len(questions)
 
 class Subsession(BaseSubsession):
-	pass
+	def before_session_starts(self):
+		if self.round_number == 1:
+			for p in self.get_players():
+				# p.participant.vars['questions'] = Constants.questions
+				p.participant.vars['questions'] = r.sample(Constants.questions, len(Constants.questions))
+				p.score = 0
+
+		for p in self.get_players():
+			question_data = p.current_question()
+			p.question_id = question_data['id']
+			p.solution = question_data['solution']
+			p.num_choices = question_data['num_choices']
+
 
 
 class Group(BaseGroup):
@@ -27,4 +48,21 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-	pass
+	question_id = models.PositiveIntegerField()
+	num_choices = models.PositiveIntegerField()
+	solution = models.CharField()
+	score = models.FloatField()
+	submitted_answer = models.PositiveIntegerField(widget=widgets.RadioSelect())
+	is_correct = models.BooleanField()
+	## These are variables of players, so they need to be defined under Player. Their values will either be assigned in Subsession or submitted through forms.
+
+	def current_question(self):
+		return self.participant.vars['questions'][self.round_number - 1]
+	## current_question need not be stored in the result table
+
+	def check_correct(self):
+		self.is_correct = self.submitted_answer == self.solution
+		if self.is_correct:
+			self.score += Constants.correct_point
+		else:
+			self.score += Constants.wrong_point
--- updatingOnAbility_tests/views.py
+++ updatingOnAbility_tests/views.py
@@ -2,6 +2,7 @@ from otree.api import Currency as c, currency_range
 from . import models
 from ._builtin import Page, WaitPage
 from .models import Constants
+import time
 
 
 class welcome(Page):
@@ -10,8 +11,39 @@ class welcome(Page):
 class IRB(Page):
 	pass
 
+class Start(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+    def before_next_page(self):
+        # user has 5 minutes to complete as many pages as possible
+        self.participant.vars['expiry_timestamp'] = time.time() + Constants.test_minutes * 60
+
+class Question(Page):
+	def get_timeout_seconds(self):
+		return self.participant.vars['expiry_timestamp'] - time.time()
+
+	def is_displayed(self):
+		return self.participant.vars['expiry_timestamp'] - time.time() > 3
+
+	form_model = models.Player
+	form_fields = ['submitted_answer']
+
+	def submitted_answer_choices(self):
+		return [k for k in range(1, self.player.num_choices+1)]
+
+	def before_next_page(self):
+		if not self.timeout_happened:
+			self.player.check_correct()
+		
+	def vars_for_template(self):
+		return {
+			'image': 'raven/raven{}.png'.format(self.player.question_id)
+		}
 
 page_sequence = [
-	welcome,
-	IRB,
+	# welcome,
+	# IRB,
+	Start,
+	Question,
 ]
